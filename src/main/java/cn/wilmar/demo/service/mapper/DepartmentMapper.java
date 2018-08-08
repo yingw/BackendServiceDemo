5016d997270e8fc5190f78d967124f023441d693
yinguowei <yinguowei@gmail.com>
2018-08-08 13:53:40 +0800

    add HR models support.

---
+++ src/main/java/cn/wilmar/demo/service/mapper/DepartmentMapper.java
@@ -0,0 +1,29 @@
+package cn.wilmar.demo.service.mapper;
+
+import cn.wilmar.demo.domain.*;
+import cn.wilmar.demo.service.dto.DepartmentDTO;
+
+import org.mapstruct.*;
+
+/**
+ * Mapper for the entity Department and its DTO DepartmentDTO.
+ */
+@Mapper(componentModel = "spring", uses = {LocationMapper.class})
+public interface DepartmentMapper extends EntityMapper<DepartmentDTO, Department> {
+
+    @Mapping(source = "location.id", target = "locationId")
+    DepartmentDTO toDto(Department department);
+
+    @Mapping(source = "locationId", target = "location")
+    @Mapping(target = "employees", ignore = true)
+    Department toEntity(DepartmentDTO departmentDTO);
+
+    default Department fromId(Long id) {
+        if (id == null) {
+            return null;
+        }
+        Department department = new Department();
+        department.setId(id);
+        return department;
+    }
+}
---
+++ src/main/java/cn/wilmar/demo/service/mapper/RegionMapper.java
@@ -0,0 +1,24 @@
+package cn.wilmar.demo.service.mapper;
+
+import cn.wilmar.demo.domain.*;
+import cn.wilmar.demo.service.dto.RegionDTO;
+
+import org.mapstruct.*;
+
+/**
+ * Mapper for the entity Region and its DTO RegionDTO.
+ */
+@Mapper(componentModel = "spring", uses = {})
+public interface RegionMapper extends EntityMapper<RegionDTO, Region> {
+
+
+
+    default Region fromId(Long id) {
+        if (id == null) {
+            return null;
+        }
+        Region region = new Region();
+        region.setId(id);
+        return region;
+    }
+}
