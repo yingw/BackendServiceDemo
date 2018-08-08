8c29cc9341d2ebe232167be308e1aa85393f8471
yinguowei <yinguowei@gmail.com>
2018-08-08 13:52:32 +0800

    add swagger UI support.

---
+++ src/main/java/cn/wilmar/demo/config/SwaggerConfig.java
@@ -0,0 +1,14 @@
+package cn.wilmar.demo.config;
+
+//import com.spring4all.swagger.EnableSwagger2Doc;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Yin Guo Wei 2018/7/30.
+ */
+// Swagger
+//@Configuration
+//@EnableSwagger2
+//@EnableSwagger2Doc
+public class SwaggerConfig {
+}
